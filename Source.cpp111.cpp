98991859f36670bae5c768204c823b0ad01989b3
dominikbobrovski <91685863+dominikbobrovski@users.noreply.github.com>
2021-10-21 16:56:58 +0300

    dominik П43021
    sm2

---
+++ Source.cpp111.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a;
+	cin >> a;
+	if (a == 1)
+		cout << "31";
+	if (a == 2) 
+		cout << "28";
+	if (a == 3)
+		cout << "31";
+	if (a == 4)
+		cout << "30";
+	if (a == 5)
+		cout << "31";
+	if (a == 6)
+		cout << "30";
+	if (a == 7)
+		cout << "31";
+	if (a == 8)
+		cout << "31";
+	if (a == 9)
+		cout << "30";
+	if (a == 10)
+		cout << "31";
+	if (a == 11)
+		cout << "30";
+	if (a == 12)
+		cout << "31";
+	return 0;
+
+}
